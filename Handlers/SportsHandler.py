5d12fcd3d79576d644887e16bebaaf78da6914ec
emeric254 <emeric254@live.fr>
2017-03-22 16:43:51 +0100

    sport list

---
+++ Handlers/SportsHandler.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import uuid
+import json
+import redis
+import logging
+from tornado import escape, web, websocket
+from Handlers.BaseHandler import BaseHandler
+
+logger = logging.getLogger(__name__)
+
+
+class SportsHandler(BaseHandler):
+    """SportsHandler
+    """
+    def initialize(self, redis_client: redis.Redis):
+        """initialize
+
+        :param redis_client: redis connection
+        """
+        self.redis_client = redis_client
+
+
+    @web.authenticated
+    def get(self):
+        self.write(self.redis_client.get('sports'))
